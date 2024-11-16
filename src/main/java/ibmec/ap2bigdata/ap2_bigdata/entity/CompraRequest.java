f4ad492f32865432f7c472f366706a318425d609
Guilherme <guibma@hotmail.com>
2024-11-16 17:22:46 -0300

    alteracao das urls

--- src/main/java/ibmec/ap2bigdata/ap2_bigdata/entity/CompraRequest.java
+++
@@ -1,9 +0,0 @@
-package ibmec.ap2bigdata.ap2_bigdata.entity;
-import lombok.Data;
-import java.util.List;
-
-@Data
-public class CompraRequest {
-    private String clienteId;
-    private List<String> produtosIds;
-}
