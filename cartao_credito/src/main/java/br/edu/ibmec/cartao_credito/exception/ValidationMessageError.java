9a21f969b81b055300542b41a9af443f4477315e
Luis Carlos Pastura Macedo <luispasturamacedo@gmail.com>
2024-11-19 13:53:38 -0300

    atttualizando validationns

--- cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/exception/ValidationMessageError.java
+++ cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/exception/ValidationMessageError.java
@@ -1,12 +1,12 @@
 package br.edu.ibmec.cartao_credito.exception;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.Data;
-
 @Data
-public class ValidationMessageErrors {
-    private String message = "Existem erros na requisições";
+public class ValidationMessageError {
+    private String message = "Sua requisição possui erros. Por favor, verifique e tente novamente.";
     private List<ValidationError> errors = new ArrayList<>();
 }
