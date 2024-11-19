13a2e80d7b65cfbdacdb50f4f3c1d39806496d90
Luis Carlos Pastura Macedo <luispasturamacedo@gmail.com>
2024-11-19 13:55:03 -0300

    update no cosmos

--- ecommerce/src/main/java/br/edu/ibmec/cloud/ecommerce/config/CosmosConfiguration.java
+++ ecommerce/src/main/java/br/edu/ibmec/cloud/ecommerce/config/CosmosConfiguration.java
@@ -1,15 +1,17 @@
 package br.edu.ibmec.cloud.ecommerce.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
 import com.azure.cosmos.CosmosClientBuilder;
 import com.azure.cosmos.DirectConnectionConfig;
 import com.azure.spring.data.cosmos.config.AbstractCosmosConfiguration;
 import com.azure.spring.data.cosmos.config.CosmosConfig;
 import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableConfigurationProperties(CosmosProperties.class)
@@ -17,7 +19,7 @@ import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
 @PropertySource("classpath:application.properties")
 public class CosmosConfiguration extends AbstractCosmosConfiguration {
     private CosmosProperties properties;
-    
+
     CosmosConfiguration(CosmosProperties properties) {
         this.properties = properties;
     }
@@ -30,16 +32,16 @@ public class CosmosConfiguration extends AbstractCosmosConfiguration {
     @Bean
     public CosmosClientBuilder cosmosBuildClient() {
         return new CosmosClientBuilder()
-                   .endpoint(this.properties.getUri()) 
-                   .key(this.properties.getKey())
-                   .directMode(DirectConnectionConfig.getDefaultConfig());
+                .endpoint(this.properties.getUri())
+                .key(this.properties.getKey())
+                .directMode(DirectConnectionConfig.getDefaultConfig());
 
     }
 
     @Bean
     public CosmosConfig cosmosConfig() {
         return CosmosConfig.builder()
-                           .enableQueryMetrics(properties.isQueryMetricsEnabled())
-                           .build();
+                .enableQueryMetrics(properties.isQueryMetricsEnabled())
+                .build();
     }
 }
