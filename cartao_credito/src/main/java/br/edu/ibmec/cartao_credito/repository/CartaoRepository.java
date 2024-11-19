ff39b854884d57d927f4a44c87666a5941685c95
Luis Carlos Pastura Macedo <luispasturamacedo@gmail.com>
2024-11-19 13:54:37 -0300

    consertando pasta repository

--- cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/repository/CartaoRepository.java
+++ cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/repository/CartaoRepository.java
@@ -1,11 +1,13 @@
 package br.edu.ibmec.cartao_credito.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
-import br.edu.ibmec.cartao_credito.model.Cartao;
+import br.edu.ibmec.projeto_cloud.model.Cartao;
 
 @Repository
 public interface CartaoRepository extends JpaRepository<Cartao, Integer> {
-    
-}
+    Cartao findByNumeroCartao(String numeroCartao);
+}  
+
--- cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/repository/TransacaoRepository.java
+++ cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/repository/TransacaoRepository.java
@@ -1,11 +1,15 @@
 package br.edu.ibmec.cartao_credito.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
-import br.edu.ibmec.cartao_credito.model.Transacao;
+import br.edu.ibmec.projeto_cloud.model.Transacao;
+
+import java.util.List;
 
 @Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Integer> {
+    List<Transacao> findByValorAndComerciante(double valor, String comerciante);
 
 }
