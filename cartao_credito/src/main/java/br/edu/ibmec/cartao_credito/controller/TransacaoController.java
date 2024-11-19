ea45c681d0a1152e4cbb37348cf31d6b086ab96d
Luis Carlos Pastura Macedo <luispasturamacedo@gmail.com>
2024-11-19 13:53:07 -0300

    ajustando controller

--- cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/controller/TransacaoController.java
+++ cartao_credito/src/main/java/br/edu/ibmec/cartao_credito/controller/TransacaoController.java
@@ -1,64 +1,82 @@
 package br.edu.ibmec.cartao_credito.controller;
 
-import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import br.edu.ibmec.projeto_cloud.service.TransacaoService;
+import br.edu.ibmec.projeto_cloud.exception.ClienteException;
+import br.edu.ibmec.projeto_cloud.model.Cartao;
+import br.edu.ibmec.projeto_cloud.model.Cliente;
+import br.edu.ibmec.projeto_cloud.model.Transacao;
+import br.edu.ibmec.projeto_cloud.repository.ClienteRepository;
+import br.edu.ibmec.projeto_cloud.repository.TransacaoRepository;
+import br.edu.ibmec.projeto_cloud.request.TransacaoRequest;
+import br.edu.ibmec.projeto_cloud.request.TransacaoResponse;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.edu.ibmec.cartao_credito.exception.UsuarioException;
-import br.edu.ibmec.cartao_credito.model.Cartao;
-import br.edu.ibmec.cartao_credito.model.Transacao;
-import br.edu.ibmec.cartao_credito.model.Usuario;
-import br.edu.ibmec.cartao_credito.request.TransacaoRequest;
-import br.edu.ibmec.cartao_credito.request.TransacaoResponse;
-import br.edu.ibmec.cartao_credito.service.TransacaoService;
-import br.edu.ibmec.cartao_credito.service.UsuarioService;
-
 @RestController
-@RequestMapping("/autorizar")
+@RequestMapping("/transacao")
 public class TransacaoController {
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+
     @Autowired
     private TransacaoService transacaoService;
 
     @Autowired
-    private UsuarioService usuarioService;
+    private ClienteRepository clienteRepository;
+
+
+    @GetMapping("{id}")
+    public ResponseEntity<Transacao> getTransacaoById(@PathVariable("id") int id) {
+        Optional<Transacao> tryResponse = transacaoRepository.findById(id);
+
+        if (!tryResponse.isPresent())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        Transacao response = tryResponse.get();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("extrato-cartao/{id}")
+    public ResponseEntity<List<Transacao>> getTransacoesByCartao(@PathVariable("id") int id) throws Exception {
+        List<Transacao> transacoes = transacaoService.getExtratoByCartao(id);
+
+        if (transacoes == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(transacoes, HttpStatus.OK);
+    }
 
     @PostMapping()
-    public ResponseEntity<TransacaoResponse> transacionar(@RequestBody TransacaoRequest request) throws Exception {
+    public ResponseEntity<TransacaoResponse> saveTransacao(@RequestBody TransacaoRequest request) throws Exception {
 
-        // Buscando o usuario
-        Usuario user = this.usuarioService.buscaUsuario(request.getIdUsuario());
+        Optional<Cliente> optCliente = clienteRepository.findById(request.getIdCliente());
 
-        if (user == null)
-            throw new UsuarioException("Usuario não encontrado, verifique o identificador");
+        if (!optCliente.isPresent())
+            throw new ClienteException("Usuario não encontrado, verifique o identificador.");
+        
+        Cliente cliente = optCliente.get();
 
-        Optional<Cartao> optCartao = user.getCartoes()
+        Optional<Cartao> optCartao = cliente.getCartoes()
                 .stream()
-                .filter(x -> x.numero.equals(request.getNumeroCartao()))
+                .filter(x -> x.getId() == (request.getIdCartao()))
                 .findFirst();
-        // Não achei o cartao de credito do usuário
-        if (optCartao.isPresent() == false)
-            throw new UsuarioException(
-                    "Não encontrei o cartão associado a esse usuário com o numero " + request.getNumeroCartao());
 
-        // Cartao do usuário
+        if (!optCartao.isPresent())
+            throw new ClienteException("Cartão de id " + request.getIdCartao() + " não está associado ao cliente de id " + request.getIdCliente() + " ou não existe.");
+
         Cartao cartao = optCartao.get();
         TransacaoResponse response = new TransacaoResponse();
 
         try {
-            Transacao transacao = this.transacaoService.autorizacaoTransacao(cartao, request.getValor(),
-                    request.getComerciante());
-            // Objeto de resposta do usuario
+            Transacao transacao = transacaoService.createTransacao(cartao.getId(), request.getValor(), request.getComerciante(), request.getDataTransacao());
+
             response.setDataTransacao(transacao.getDataTransacao());
             response.setStatus("APROVADO");
             response.setValor(transacao.getValor());
@@ -66,39 +84,12 @@ public class TransacaoController {
 
         } catch (Exception e) {
             response.setStatus("REPROVADO:" + e.getMessage());
-            response.setDataTransacao(LocalDateTime.now());
+            response.setDataTransacao(request.getDataTransacao());
+            response.setValor(request.getValor());
+            response.setErro(e.getMessage());
         }
 
         return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @GetMapping("{id}")
-    public ResponseEntity<List<Transacao>> extratoCartao(@PathVariable("id") int id, String numeroCartao)
-            throws Exception {
-        // Buscando o usuario
-        Usuario user = this.usuarioService.buscaUsuario(id);
-
-        if (user == null)
-            throw new UsuarioException("Usuario não encontrado, verifique o identificador");
-
-        Optional<Cartao> optCartao = user.getCartoes()
-                .stream()
-                .filter(x -> x.numero.equals(numeroCartao))
-                .findFirst();
-
-        // Não achei o cartao de credito do usuário
-        if (optCartao.isPresent() == false)
-            throw new UsuarioException(
-                    "Não encontrei o cartão associado a esse usuário com o numero " + numeroCartao);
-        
-        // Pega as transações corrente do cartão de credito pelo ano e mes
-        List<Transacao> transacaos = optCartao.get()
-                                              .getTransacoes()
-                                              .stream()
-                                              .filter(x -> x.dataTransacao.getMonth() == LocalDateTime.now().getMonth() && x.dataTransacao.getYear() == LocalDateTime.now().getYear())
-                                              .toList();
-
-        return new ResponseEntity<>(transacaos, HttpStatus.OK);                    
     }
 
 }
