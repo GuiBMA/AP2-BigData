f1012a1e04ed8900a5e6f3fd98f1bb4a9811a0b3
Guilherme <guibma@hotmail.com>
2024-11-25 12:52:42 -0300

    correcao nos controllers

--- ecommerce/src/main/java/br/edu/ibmec/cloud/ecommerce/controller/OrderController.java
+++ ecommerce/src/main/java/br/edu/ibmec/cloud/ecommerce/controller/OrderController.java
@@ -1,9 +1,6 @@
 package br.edu.ibmec.cloud.ecommerce.controller;
-
 import br.edu.ibmec.cloud.ecommerce.entity.Order;
-import br.edu.ibmec.cloud.ecommerce.entity.Pedido;
-import br.edu.ibmec.cloud.ecommerce.entity.Produto;
-import br.edu.ibmec.cloud.ecommerce.service.ProdutoService;
+import br.edu.ibmec.cloud.ecommerce.service.OrderService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,43 +13,26 @@ import java.util.List;
 public class OrderController {
 
     @Autowired
-    private ProdutoService service;
+    private OrderService orderService; // Serviço correto para "Order"
 
     @PostMapping
     public ResponseEntity<Order> create(@Valid @RequestBody Order order) {
-        this.service.save(order);
+        this.orderService.save(order);
         return new ResponseEntity<>(order, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/idOrder/{id}")
-    public ResponseEntity<List<Produto>> getByidProduto(@PathVariable("idProduto") String id) {
-        List<Produto> produtos = this.service.findOrderById(id);
+    public ResponseEntity<List<Order>> getByidProduto(@PathVariable("idProduto") String id) {
+        List<Order> orders = this.orderService.findOrderById(id);
 
-        return new ResponseEntity<>(produtos, HttpStatus.OK);
+        return new ResponseEntity<>(orders, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/idProduto/{id}")
-    public ResponseEntity<List<Produto>> getOrderByidProduto(@PathVariable("idProduto") String id) {
-        List<Produto> produtos = this.service.findByidProduto(id);
+    // @GetMapping(value = "/idProduto/{id}")
+    // public ResponseEntity<List<Order>> getOrderByidProduto(@PathVariable("idProduto") String id) {
+    //     List<Order> orders = this.orderService.findByidProduto(id);
 
-        return new ResponseEntity<>(produtos, HttpStatus.OK);
-    }
-
-    // @GetMapping
-    // public ResponseEntity<List<Order>> getByProductName(@RequestParam String nomeProduto) {
-    //     List<Order> produtos = this.service.findProductByName(nomeProduto);
-    //     return new ResponseEntity<>(produtos, HttpStatus.OK);
+    //     return new ResponseEntity<>(orders, HttpStatus.OK);
     // }
 
-    // @GetMapping("/todos")
-    // public ResponseEntity<List<Order>> getAllProducts() {
-    //     List<Produto> produtos = this.service.buscarTodosProdutos(); // Busca todos os produtos do serviço
-    //     return new ResponseEntity<>(produtos, HttpStatus.OK);
-    // }
-
-    // @DeleteMapping("{id}")
-    // public ResponseEntity<Void> delete(@PathVariable("id") String id) throws Exception{
-    //     this.service.delete(id);
-    //     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    // }
 }
